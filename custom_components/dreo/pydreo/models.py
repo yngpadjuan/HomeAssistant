bb387f1df5bb8e9c0002f890468a22d5ace2bf68
David Denzel <ddenzel@splunk.com>
2026-08-31 14:54:38 -0600

    added DR-HCF010S to models.py

--- custom_components/dreo/pydreo/models.py
+++ custom_components/dreo/pydreo/models.py
@@ -428,6 +428,11 @@ SUPPORTED_DEVICES = {
         preset_modes=[("normal", 1), ("natural", 2), ("sleep", 3), ("reverse", 4)],
         device_ranges={SPEED_RANGE: (1, 12)},
     ),
+    "DR-HCF010S": DreoDeviceDetails(
+        device_type=DreoDeviceType.CEILING_FAN,
+        preset_modes=[("normal", 1), ("natural", 2), ("sleep", 3), ("reverse", 4)],
+        device_ranges={SPEED_RANGE: (1, 12)},
+    ),
     "DR-HCF002S": DreoDeviceDetails(device_type=DreoDeviceType.CEILING_FAN, device_ranges={SPEED_RANGE: (1, 12)}),
     "DR-HCF007S": DreoDeviceDetails(
         device_type=DreoDeviceType.CEILING_FAN,
